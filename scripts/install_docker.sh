963d2161bcf7a0b0fd4afdffd1a28ec34e92c2fd
ahsan0786 <a14ahsiqbiqb@iam.cat>
2018-05-03 16:22:47 +0200

    ok

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -7,4 +7,6 @@ apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
 su - ${USER}
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
