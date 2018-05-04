e4a1e4b28efd3160e3d26557d7c502cd31d42204
ahsan0786 <a14ahsiqbiqb2iam.cat>
2018-05-04 16:48:03 +0200

    ok

--- scripts/add_jenkinscluster_ubuntu3.sh
+++ scripts/add_jenkinscluster_ubuntu3.sh
@@ -1,6 +1,7 @@
-echo "pega el comando docker swarm join...."
-read swarm
-bash -c "$swarm"
+#echo "pega el comando docker swarm join...."
+#read swarm
+#bash -c "$swarm"
+docker swarm init
 echo "IP jenkins master"
 read ipmaster
 echo "username jenkins master"
