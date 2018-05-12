47b40aab4df1a87bded519738476cce19cacd5bd
ahsan0786 <a14ahsiqbiqb@iam.cat>
2018-05-12 15:27:24 +0200

    ok

--- scripts/chown+mkdir.sh
+++ scripts/chown+mkdir.sh
@@ -6,10 +6,12 @@ if [ -f /home/ubuntu/docker/containers/mysql ] && [ -f /home/ubuntu/docker/conta
         sudo rm -r /home/ubuntu/docker/containers/mysql
         sudo rm -r /home/ubuntu/docker/containers/wordpress
         sudo rm -r /home/ubuntu/docker/containers/mysql-config
-else 
+else
         sudo mkdir /home/ubuntu/docker/containers/mysql
         sudo mkdir /home/ubuntu/docker/containers/mysql-config
         sudo mkdir /home/ubuntu/docker/containers/wordpress
-        sudo cp -a ${HOME}/proyecto/config/my.cnf /home/ubuntu/docker/containers/mysql-config 
+        sudo cp -a ${HOME}/proyecto/config/my.cnf /home/ubuntu/docker/containers/mysql-config
+        sudo cp -a ${HOME}/proyecto/config/my2.cnf /home/ubuntu/docker/containers/mysql-config
+
 fi
 
