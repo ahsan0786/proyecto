964033177dd93ac9f72368960438fb3bc1edaa44
root <root@jenkins-slave3_prod.local>
2018-05-19 12:47:09 +0200

    ok

--- scripts/chown+mkdir.sh
+++ scripts/chown+mkdir.sh
@@ -6,6 +6,12 @@ if [ -d /home/ubuntu/docker/containers/mysql ] && [ -d /home/ubuntu/docker/conta
         sudo rm -r /home/ubuntu/docker/containers/mysql
         sudo rm -r /home/ubuntu/docker/containers/wordpress
         sudo rm -r /home/ubuntu/docker/containers/mysql-config
+        sudo mkdir /home/ubuntu/docker/containers/mysql
+        sudo mkdir /home/ubuntu/docker/containers/mysql-config
+        sudo mkdir /home/ubuntu/docker/containers/wordpress
+        sudo cp -a ${HOME}/proyecto/config/my.cnf /home/ubuntu/docker/containers/mysql-config
+        sudo cp -a ${HOME}/proyecto/config/my2.cnf /home/ubuntu/docker/containers/mysql-config
+
 else
         sudo mkdir /home/ubuntu/docker/containers/mysql
         sudo mkdir /home/ubuntu/docker/containers/mysql-config
