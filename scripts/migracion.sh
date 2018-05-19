18464c6cd270181d307467f70d1eb2f0c359d275
ahsan0786 <a14ahsiqbiqb@iam.cat>
2018-05-19 20:04:25 +0200

    ok

--- scripts/migracion.sh
+++ scripts/migracion.sh
@@ -30,8 +30,8 @@ read WP_CONTENT_CLIENTE
 echo "Nombre de usuario de la máquina"
 read USUARIO_MAQUINA_CLIENTE
 
-rsync -avzhe ssh $USUARIO_MAQUINA_CLIENTE@$WORDPRESS_IP_CLIENTE:$WP_CONTENT_CLIENTE ./wordpress
-# peticion de variables de la maquina migracion
+rsync -avzhe ssh $USUARIO_MAQUINA_CLIENTE@$WORDPRESS_IP_CLIENTE:$WP_CONTENT_CLIENTE ./wp-content
+#peticion de variables de la maquina migracion
 
 echo "Usuario máquina migracion"
 read USUARIO_MAQUINA_MIGRACION
@@ -46,13 +46,10 @@ read WORDPRESS_IP_MIGRACION
 
 #inserccion de la copia de seguridad a la maquina migracion
 #rsync -avzhe ssh $NOMBRE_DB_CLIENTE.sql $USUARIO_MAQUINA_MIGRACION@$MYSQL_IP_MIGRACION:$PATH_DB_MIGRACION
-
+mysql --host $MYSQL_IP_MIGRACION -uroot -p$PASS_DB_MIGRACION -e 'DROP DATABASE wordpress';
+mysql --host $MYSQL_IP_MIGRACION -uroot -p$PASS_DB_MIGRACION -e 'CREATE DATABASE wordpress';
 mysql -h $MYSQL_IP_MIGRACION -u root -p  $NOMBRE_DB_CLIENTE < $NOMBRE_DB_CLIENTE.sql
-#if [ -d /home/ubuntu/docker/containers/wordpress ]; then
-#	sudo rm -r /home/ubuntu/docker/containers/wordpress 
-#else
-#	sudo mkdir /home/ubuntu/docker/containers/wordpress
-rsync -avzhe ssh ./wordpress/wordpress $USUARIO_MAQUINA_MIGRACION@$WORDPRESS_IP_CLIENTE:/home/ubuntu/docker/containers/wordpress
+#sincronizacion de la carpeta wordpress a la maquina master
+ssh -t $USUARIO_MAQUINA_MIGRACION@$WORDPRESS_IP_MIGRACION "/usr/bin/sudo rm -r /home/ubuntu/docker/containers/wordpress/wp-content && /usr/bin/sudo mkdir /home/ubuntu/docker/containers/wordpress/wp-content && /usr/bin/sudo chmod 777 /home/ubuntu/docker/containers/wordpress/wp-content "
+rsync -avzhe ssh ./wp-content/ $USUARIO_MAQUINA_MIGRACION@$WORDPRESS_IP_MIGRACION:/home/ubuntu/docker/containers/wordpress/wp-content/
 
-
-#rsync -avzhe ssh backup.tar root@192.168.0.100:/backups/
