dde8790f04bea4b970216eea0cc7ae612c3d5e40
ahsan0786 <a14ahsiqbiqb@iam.cat>
2018-05-19 11:03:40 +0200

    ok

--- scripts/chown+mkdir.sh
+++ scripts/chown+mkdir.sh
@@ -2,7 +2,7 @@
 cd ${HOME}
 sudo rm -r ./proyecto
 git clone https://github.com/ahsan0786/proyecto.git
-if [ -f /home/ubuntu/docker/containers/mysql ] && [ -f /home/ubuntu/docker/containers/wordpress ] && [ -f /home/ubuntu/docker/containers/mysql-config ]; then
+if [ -d /home/ubuntu/docker/containers/mysql ] && [ -d /home/ubuntu/docker/containers/wordpress ] && [ -d /home/ubuntu/docker/containers/mysql-config ]; then
         sudo rm -r /home/ubuntu/docker/containers/mysql
         sudo rm -r /home/ubuntu/docker/containers/wordpress
         sudo rm -r /home/ubuntu/docker/containers/mysql-config
